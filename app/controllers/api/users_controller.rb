48b9559e3bea92852d01e33efb252c7bebdc80e7
ShaundaK <sdking1913@gmail.com>
2018-11-10 09:14:32 -0600

    add seed file users api index works

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,6 @@
+class Api::UsersController < ApplicationController
+  def index 
+    @users = User.all
+    render "index.json.jbuilder"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     post "/products" => "products#create"
     patch "/products/:id" => "products#update"
     delete "/products/:id" => "products#destroy"
+
+    get "/users" => "users#index"
   end
 end
