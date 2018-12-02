5ff45bf736212873288f4575bac3bee7b5f2be51
ShaundaK <sdking1913@gmail.com>
2018-12-02 14:36:30 -0600

    remove category id from product table and asscoiate products and categories

--- app/views/api/categories/show.json.jbuilder
+++ app/views/api/categories/show.json.jbuilder
@@ -1,5 +1,5 @@
 json.id @category.id
 json.name @category.name
-json.product @category.products
+json.products @category.products
 json.created_at @category.created_at
 json.updated_at @category.updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_200657) do
+ActiveRecord::Schema.define(version: 2018_12_02_200822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2018_11_25_200657) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
