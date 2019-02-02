132746cb1da7579e1ffd94a389cb435b0a1c46e4
ShaundaK <sdking1913@gmail.com>
2019-02-01 19:16:28 -0600

    change cors.rb again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'localhost:8080'
       origins 'https://little-elephant.herokuapp.com/'
+      
 
     resource '*',
       headers: :any,
